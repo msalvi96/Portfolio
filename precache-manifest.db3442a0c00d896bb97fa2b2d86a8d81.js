71d3ed9ae98e2a07345825396c586f47bad134c9
Mrunal <msalvi96@stevens.edu>
2020-02-01 18:18:36 -0500

    Updates

---
+++ precache-manifest.db3442a0c00d896bb97fa2b2d86a8d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cd589b9320531d54a70c294d14bb1c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "982bf96c29d9017d5883",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4c19723d0ad2a5d93a90",
+    "url": "/Portfolio/static/js/2.4f24d67f.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/Portfolio/static/js/2.4f24d67f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "982bf96c29d9017d5883",
+    "url": "/Portfolio/static/js/main.0effee93.chunk.js"
+  },
+  {
+    "revision": "fc5338d523801bf5e718",
+    "url": "/Portfolio/static/js/runtime-main.4005ac6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.4512e601c7212872859c11781c3a8667.js"
+  "/Portfolio/precache-manifest.db3442a0c00d896bb97fa2b2d86a8d81.js"
 );
 
 self.addEventListener('message', (event) => {
